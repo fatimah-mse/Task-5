d5cb9bb053116d20f87844c286bd41bee60f4f51
fatimah-mse <fatimahmse83@gmail.com>
2024-04-09 02:29:27 +0200

    modified-small-details

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,6 +18,7 @@ let brand_3 = document.getElementById('brand-3');
 let brand_4 = document.getElementById('brand-4');
 
 let text = document.getElementById('text');
+let location_icon = document.getElementById('location-icon');
 let map = document.getElementById('map');
 
 window.onscroll = function () {
@@ -134,4 +135,5 @@ function showmap() {
     .35850965675029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1523fe3b216d9809%3A0x5f2b01a0fb9029d1!2sAl%
     20Jarkas%20Neighborhood%2C%20Jableh%2C%20Syria!5e0!3m2!1sen!2s!4v1707920354699!5m2!1sen!2s`;
     text.style.display = 'none';
+    location_icon.style.display = 'none';
 }
